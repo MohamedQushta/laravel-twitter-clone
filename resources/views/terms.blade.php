dfbf91185119662eae34282c9053cc362a28a25d
Qushta <m.ashraf2121@nu.edu.eg>
2024-06-23 15:05:25 +0300

    using layouts and templates

---
+++ resources/views/terms.blade.php
@@ -0,0 +1,11 @@
+@extends('layout.layout')
+
+@section('content')
+<div>
+    <h1>Terms</h1>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa sunt, eaque exercitationem optio consequatur est repudiandae porro recusandae ut dicta. Deleniti quod beatae, exercitationem, neque quisquam iure eum accusamus adipisci, sed ratione eius omnis itaque odit. Est, ullam officiis ab tenetur aperiam voluptatem obcaecati enim unde harum consequatur delectus architecto magni adipisci quam voluptatum omnis fugiat ipsum maiores voluptate blanditiis assumenda! Corrupti ullam soluta fugiat eveniet, asperiores dolorum autem a provident, dicta adipisci impedit! Quibusdam reiciendis blanditiis enim eos nostrum vitae earum tempore maiores sit deserunt nam est reprehenderit iure non voluptatum cupiditate nisi numquam modi ab veritatis, omnis iusto?
+    </div>
+</div>
+@endsection
+
--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [DashboardController::class , 'index']); //this accesses the index function in the dashboard controller
+
+Route::get('/terms', function(){
+    return view('terms');
+});
