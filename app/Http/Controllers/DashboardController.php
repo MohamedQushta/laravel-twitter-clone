90c64c03d97355ff7d575957b1125f9ec05c3b9e
Qushta <m.ashraf2121@nu.edu.eg>
2024-06-24 10:24:45 +0300

    migrations on database

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,32 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Idea;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    //this function will send the users array from the
-    //controller to the view (html/css)
-    public function index(){
-        $users = [
-            [
-                'name' => 'Alex',
-                'age' => 30,
-            ],
-            [
-                'name'  => 'Dan',
-                'age' => 25,
-            ],
-            [
-                'name' => 'Mohamed',
-                'age' => 17,
-            ]
-            ];
-        return view(
-            'dashboard',
-            [
-                'usersList' => $users
-            ]
-            );
+    public function index()
+    {
+        $idea = new Idea(
+            ['content' => 'hi',]
+        );
+        $idea->save();
+        return view('dashboard', [
+            'ideas' => Idea::orderBy('created_at','DESC')->get()
+        ]);
     }
 }
